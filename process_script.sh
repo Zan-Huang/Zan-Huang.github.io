69bb61dcafd9966c407fd89c6b7ea2e4c0305e59
zanh <zanh@umich.edu>
2022-12-09 13:02:47 -0500

    update site

--- process_script.sh
+++ process_script.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-cd documents
-for f in *
-do
-	output="($f).md"
-	pandoc --read=rtf --write=markdown -s $f -o $output
-	cp "$output" ../blags/content/posts
-done
-
-cd ..
-
 cd blags
 blag build
 mv -v build/atom.xml ..
