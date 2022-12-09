a1992665697fd4a6d2e57adc61f15fa884f9b1af
zanh <zanh@umich.edu>
2022-12-09 13:49:25 -0500

    update site

--- process_script.sh
+++ process_script.sh
@@ -6,7 +6,7 @@ mv -v build/tags ..
 cd ..
 cd blags/build
 insertVAR="<link rel="stylesheet" href="style.css" type="text/css">"
-sed -i'3\$insertVAR' index.html
+sed  '/\[<head>\]/a $insertVAR' index.html
 cd ..
 cd ..
 git add .
