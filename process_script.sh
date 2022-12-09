2909b5eaea6b9957f639de1e1cf5ff1c2b65e8a2
zanh <zanh@umich.edu>
2022-12-09 13:39:20 -0500

    update site

--- process_script.sh
+++ process_script.sh
@@ -4,9 +4,10 @@ blag build
 mv -v build/atom.xml ..
 mv -v build/tags ..
 cd ..
-for insert in /blags/build/*.html; do
-    sed '3 i <link rel="stylesheet" href="/style.css" type="text/css">' "insert"
-done
+cd blags/build
+sed '3 i <link rel="stylesheet" href="style.css" type="text/css">' "insert"
+cd ..
+cd ..
 git add .
 git commit -m "update site"
 git push origin
